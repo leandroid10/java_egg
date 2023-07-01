1e2d155384829226953dcdfd72411f0bc9576c69
Leandro Ferreyra <leito.ferreyra@gmail.com>
2023-06-30 21:23:47 -0300

    Ejercicio 1 de la guia 2

---
+++ guia2/src/guia2m1/guia1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia2m1;
+
+/**
+ *
+ * @author leandro
+ */
+public class guia1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte num;
+        short num2;
+        int num3;
+        long num4;
+        float  num5;
+        double num6;
+        boolean verdadero;
+        char num7;
+        String nombre; 
+        
+     
+      
+                
+       
+    }
+    
+}
+/*Crear un proyecto de Java y definir al menos 
+6 variables en tu IDE de distintos tipos de datos.*/
